525780563415986b6356936c001f12be109e2d34
Jonathan Devere-Ellery <jonathand@microsoft.com>
2024-12-16 12:02:21 +0100

    Add parameters for connecting to 21Vianet

--- SOA/SOA-Prerequisites.psm1
+++ SOA/SOA-Prerequisites.psm1
@@ -998,7 +998,7 @@ Function Test-Connections {
             do {
                 try {
                     $connCount++
-                    Write-Verbose "$(Get-Date) Graph Delegated connection attempt #$connCount"
+                    Write-Verbose "$(Get-Date) Test-Connections: Graph Delegated connection attempt #$connCount"
                     # User.Read is sufficient for using the organization API to get the domain for the Teams/SPO connections
                     # Using Organization.Read.All because that is the least-common scope for getting licenses in the app check
 
@@ -1679,7 +1679,13 @@ Function Install-SOAPrerequisites
         [switch]$HasMDILicense,
     [Parameter(ParameterSetName='Default')]
     [Parameter(ParameterSetName='ModulesOnly')]
-        [switch]$HasTeamsLicense
+        [switch]$HasTeamsLicense,
+    [Parameter(ParameterSetName='Default')]
+    [Parameter(ParameterSetName='EntraAppOnly')]
+        $GraphClientId,
+    [Parameter(ParameterSetName='Default')]
+    [Parameter(ParameterSetName='EntraAppOnly')]
+        $TenantName
     )
 
     <#
@@ -1986,7 +1992,7 @@ Function Install-SOAPrerequisites
 
     If($ConnectCheck -eq $True) {
         # Proceed to testing connections
-        
+
         $Connections = @(Test-Connections -RPSProxySetting $RPSProxySetting -CloudEnvironment $CloudEnvironment)
         
         $Connections_OK = @($Connections | Where-Object {$_.Connected -eq $True -and $_.TestCommand -eq $True})
@@ -2005,6 +2011,14 @@ Function Install-SOAPrerequisites
             "China"        {$cloud = 'China'}
         }
 
+        if ($CloudEnvironment -eq "China" -and ($null -eq $GraphClientId -or $null -eq $TenantName)) {
+            # Check whether the Client ID has been manually provided
+            Write-Host "It's required to provide the Client ID"
+
+            Exit-Script
+            throw "$(Get-Date) Connections to Graph in 21Vianet will fail unless the Client ID and Tenant Name are manually provided. Use `-GraphClientId` and `-TenantName` parameters to provide them after the application has been manually registered in your tenant. For more information visit https://github.com/o365soa/soa."           
+        }
+
         $mgContext =  (Get-MgContext).Scopes
         if ($mgContext -notcontains 'Application.ReadWrite.All' -or ($mgContext -notcontains 'Organization.Read.All' -and $mgContext -notcontains 'Directory.Read.All') -or ($PromptForApplicationSecret)) {
             Write-Host "$(Get-Date) Connecting to Graph with delegated authentication..."
@@ -2014,8 +2028,12 @@ Function Install-SOAPrerequisites
             do {
                 try {
                     $connCount++
-                    Write-Verbose "$(Get-Date) Graph Delegated connection attempt #$connCount"
-                    if ($PromptForApplicationSecret) {
+                    Write-Verbose "$(Get-Date) Install-SOAPrerequisites: Graph Delegated connection attempt #$connCount"
+
+                    if ($CloudEnvironment -eq "China") {
+                        # Connections to 21Vianet must have provided the ClientID and Tenant manually
+                        Connect-MgGraph -Scopes 'Application.ReadWrite.All','Organization.Read.All' -Environment $cloud -ContextScope "Process" -ClientId $GraphClientId -Tenant $TenantName | Out-Null
+                    } elseif ($PromptForApplicationSecret) {
                         # Request read-only permissions to Graph if manually providing the client secret
                         Connect-MgGraph -Scopes 'Application.Read.All','Organization.Read.All' -Environment $cloud -ContextScope "Process" | Out-Null
                     } else {
